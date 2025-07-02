1e865946880d015ad96e6b1155d80cd07e00673d
Ryan Haley <me@ryanchaley.com>
2025-07-02 11:23:15 -0400

    refactor: Update to use existing server packages

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,15 +1,37 @@
 package main
 
 import (
+	"context"
+	_ "embed"
 	"fmt"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+
+	"github.com/haleyrc/log"
+	"github.com/haleyrc/router"
+	"github.com/haleyrc/server"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "OK")
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	logger := slog.New(log.NewJSONHandler(log.Debug()))
+
+	router := router.New()
+
+	router.GET("/{$}", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "OK")
 	})
-	if err := http.ListenAndServe(":8086", nil); err != nil {
-		panic(err)
+
+	var h http.Handler = router
+	h = log.ParseRequestInfo(h)
+
+	server := server.New("8086", h)
+	logger.Info("server listening", slog.String("addr", server.Addr()))
+	if err := server.ListenAndServe(ctx); err != nil {
+		logger.Error("server shutdown unexpectedly", slog.Any("error", err))
 	}
 }
